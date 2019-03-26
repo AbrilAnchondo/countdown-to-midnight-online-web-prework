fd2f33037f05a662d6c074cd3f8e44b7b01edbce
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-26 16:44:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number =  10
+  while number > 0 do 
+    puts "#{number} SECOND(S)!"
+    number _= 1 
+  end  
+  return "HAPPY NEW YEAR!"
 end
